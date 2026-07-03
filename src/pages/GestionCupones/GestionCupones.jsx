6a874bbc2500fad6bac2fa91e2fe02b589663514
Marcelo Rey <reymf1@yahoo.com.ar>
2026-07-03 15:37:07 -0300

    Avance 03/07/26

---
+++ src/pages/GestionCupones/GestionCupones.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../../firebase/config";
+import {
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  addDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { toast } from "react-toastify"; //Para usar notificaciones en lugar de alert
+
+const GestionCupones = () => {
+  const [cupones, setCupones] = useState([]);
+  const [error, setError] = useState(null);
+  const [cargando, setCargando] = useState(false);
+
+  //Cargar cupones (READ)
+  const cargarCupones = async () => {
+    try {
+      const cuponesRef = collection(db, "cupones");
+      const resp = await getDocs(cuponesRef);
+      const lista = resp.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+      setCupones(lista);
+    } catch (error) {
+      setError(`"Error al cargar los cupones: " ${error.message}`);
+    }
+  };
+
+  useEffect(() => {
+    cargarCupones();
+  }, []);
+};
+
+//Crear cupón (CREATE)
+const crearCupon = async (e) => {
+  e.preventDefault();
+};
